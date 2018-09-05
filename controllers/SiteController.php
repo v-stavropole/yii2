e58817d9095c12a1fd9af556efe080416f2ba609
v-stavropole <nick80.k@yandex.ru>
2018-09-05 17:42:21 +0300

    2018-09-05-lesson1.1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    
+    public function actionWorld(){
+        return $this->render('world');
+    }
+         
 }
